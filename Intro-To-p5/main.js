55b9416554d5159dab6e6af4f7e3e61636b880b4
Surya Dev <92857513+DyaweJhonson@users.noreply.github.com>
2022-06-07 12:17:19 +0530

    Add files via upload

---
+++ Intro-To-p5/main.js
@@ -0,0 +1,31 @@
+function preload()
+{
+
+}
+
+function setup()
+{
+    canvas=createCanvas(640,480);
+    canvas.position(375,250);
+    video=createCapture(VIDEO);
+    video.hide();
+    tint_color="";
+}
+
+function draw()
+{
+    image(video,0,0,640,480);
+    tint(tint_color);
+}
+
+function take_snapshot()
+
+{
+    save('surya.png');
+}
+
+function filter_tint()
+{
+    tint_color=document.getElementById("color_name").value;
+}
+
